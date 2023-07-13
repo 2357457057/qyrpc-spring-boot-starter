32ccf1b15111f5aaf4d9f5a6bdc8b098a42e9ed9
SeverusQy <yangyongji123@live.con>
2023-07-14 03:20:09 +0800

    对于 MybatisMapper 进行特殊的代理。

--- src/main/java/top/yqingyu/qyrpc/autoconfigure/Constants.java
+++ src/main/java/top/yqingyu/qyrpc/autoconfigure/Constants.java
@@ -3,4 +3,6 @@ package top.yqingyu.qyrpc.autoconfigure;
 public interface Constants {
     String prefix = "qyrpc";
     String mode = "mode";
+    String MapperFactoryBean = "org.mybatis.spring.mapper.MapperFactoryBean";
+    String MapperFactoryBeanInvokeMethod = "getObject";
 }
--- src/main/java/top/yqingyu/qyrpc/autoconfigure/ProducerBeanRegister.java
+++ src/main/java/top/yqingyu/qyrpc/autoconfigure/ProducerBeanRegister.java
@@ -4,17 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ResourceLoader;
 import top.yqingyu.rpc.annontation.QyRpcProducer;
 import top.yqingyu.rpc.producer.Producer;
 
-
+import java.lang.reflect.Method;
 import java.util.LinkedList;
-
 
 public class ProducerBeanRegister implements InstantiationAwareBeanPostProcessor {
     public static final Logger logger = LoggerFactory.getLogger(ProducerBeanRegister.class);
     private Producer qyrpcProducer;
-    private final LinkedList<Object> BEAN_QUEUE = new LinkedList<>();
+    final LinkedList<Object> BEAN_QUEUE = new LinkedList<>();
+    ApplicationContext ctx;
+    Class<?> mybatisMapperFactoryBeanClass;
+
+    public ProducerBeanRegister(ApplicationContext ctx) {
+        this.ctx = ctx;
+    }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
@@ -25,6 +32,15 @@ public class ProducerBeanRegister implements InstantiationAwareBeanPostProcessor
         QyRpcProducer annotation = aClass.getAnnotation(QyRpcProducer.class);
         if (annotation != null) {
             BEAN_QUEUE.add(bean);
+        }
+        if (mybatisMapperFactoryBeanClass != null && Constants.MapperFactoryBean.equals(bean.getClass().getName())) {
+            try {
+                //代理工厂创建的Mybatis的代理类
+                Method getObject = bean.getClass().getMethod(Constants.MapperFactoryBeanInvokeMethod);
+                Object invoke = getObject.invoke(bean);
+                BEAN_QUEUE.add(invoke);
+            } catch (Exception ignore) {
+            }
         }
         if (qyrpcProducer != null) {
             Object poll;
@@ -39,7 +55,6 @@ public class ProducerBeanRegister implements InstantiationAwareBeanPostProcessor
                 }
             } while (poll != null);
         }
-
-        return InstantiationAwareBeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+        return bean;
     }
 }
