d4f5f9509fd65fa15956104f7ffb4be6e08eb89a
SeverusQy <yangyongji123@live.con>
2024-02-06 20:21:57 +0800

    处理启动问题

--- src/main/java/top/yqingyu/qyrpc/autoconfigure/QyRpcAutoConfiguration.java
+++ src/main/java/top/yqingyu/qyrpc/autoconfigure/QyRpcAutoConfiguration.java
@@ -47,7 +47,7 @@ public class QyRpcAutoConfiguration implements InitializingBean {
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnBean(ServerExceptionHandler.class)
-    @ConditionalOnProperty(prefix = Constants.producerPrefix)
+    @ConditionalOnProperty(name = Constants.producerPrefix)
     public Producer qyrpcProducer(ApplicationContext ctx) throws Exception {
         ProducerConfig config = properties.getProducer();
         if (config == null) {
@@ -91,7 +91,7 @@ public class QyRpcAutoConfiguration implements InitializingBean {
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnBean({MethodExecuteInterceptor.class})
-    @ConditionalOnProperty(prefix = Constants.consumerPrefix)
+    @ConditionalOnProperty(name = Constants.consumerPrefix)
     public ConsumerHolderContext qyrpcConsumerHolderContext() throws Exception {
         ConsumerHolderContext context = new ConsumerHolderContext();
         try {
